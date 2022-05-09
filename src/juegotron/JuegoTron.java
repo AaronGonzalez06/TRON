dd9f58c84f74537abce00fdf386da497acf8a50c
AaronGonzalez06 <aaron060812060812@gmail.com>
2022-05-09 19:14:05 +0200

    menu casi acabado

--- src/juegotron/JuegoTron.java
+++ src/juegotron/JuegoTron.java
@@ -10,6 +10,10 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.Icon;
@@ -18,21 +22,27 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 /**
  *
  * @author Aaron
  */
 public class JuegoTron extends JFrame {
     
-    JLabel fondoMenuPrincipal,titulo;
+    JLabel fondoMenuPrincipal,titulo, nombrePersonaje;
+    JLabel imagen,imagen2,imagen3;
     JButton BotonEmpezar;
+    JPanel PanelPersonajes;
 
     public JuegoTron() {
         crearVentana();        
-        fondo();
+        
         boton();
-        fuente();
-
+        
+        panelPersonajes();
+        fondo();
+        accionesBotones();
+        fuente(); 
     }
 
     public void crearVentana() {
@@ -67,6 +77,7 @@ public class JuegoTron extends JFrame {
             Font sizedFontEstandar = font.deriveFont(15f);
             titulo.setFont(sizedFont);
             BotonEmpezar.setFont(sizedFontEstandar);
+            nombrePersonaje.setFont(sizedFontEstandar);
         } catch (FontFormatException ex) {
             System.err.println("error en font format");
         } catch (IOException ex) {
@@ -96,9 +107,129 @@ public class JuegoTron extends JFrame {
         BotonEmpezar.setText("Jugar");
         BotonEmpezar.setForeground(Color.red);
         BotonEmpezar.setBounds(320, 400, 110, 20);
-        this.add(BotonEmpezar);
+        this.add(BotonEmpezar);                        
     
     }
+    
+    public void panelPersonajes(){
+        
+        
+        PanelPersonajes = new JPanel();
+        PanelPersonajes.setBackground(Color.black);
+        PanelPersonajes.setOpaque(false);
+        PanelPersonajes.setBounds(230, 450, 300, 50);
+        this.add(PanelPersonajes);
+        PanelPersonajes.setVisible(false);
+        //panel acabado
+        
+        
+        //personaje 1
+         imagen = new JLabel();
+        String nombre = "asset/red.png";
+        ImageIcon imageicon = new ImageIcon(nombre);
+        Icon icon = new ImageIcon(imageicon.getImage().getScaledInstance(80, 40, Image.SCALE_DEFAULT));
+        imagen.setIcon(icon);
+        //imagen.setBounds(0, 0, 80, 40);
+        PanelPersonajes.add(imagen);
+        
+        //personaje 2
+         imagen2 = new JLabel();
+        String nombre2 = "asset/blue.png";
+        ImageIcon imageicon2 = new ImageIcon(nombre2);
+        Icon icon2 = new ImageIcon(imageicon2.getImage().getScaledInstance(80, 40, Image.SCALE_DEFAULT));
+        imagen2.setIcon(icon2);
+        //imagen.setBounds(0, 0, 80, 40);
+        PanelPersonajes.add(imagen2);
+        
+        //personaje 3
+         imagen3 = new JLabel();
+        String nombre3 = "asset/gray.png";
+        ImageIcon imageicon3 = new ImageIcon(nombre3);
+        Icon icon3 = new ImageIcon(imageicon3.getImage().getScaledInstance(80, 40, Image.SCALE_DEFAULT));
+        imagen3.setIcon(icon3);
+        //imagen.setBounds(0, 0, 80, 40);
+        PanelPersonajes.add(imagen3);
+        
+        nombrePersonaje = new JLabel();
+        //nombrePersonaje.setText("aa");
+        nombrePersonaje.setForeground(Color.red);
+        nombrePersonaje.setBounds(350, 490, 300, 50);
+        this.add(nombrePersonaje);
+    
+    }
+    
+    public void accionesBotones(){
+        BotonEmpezar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {                
+            PanelPersonajes.setVisible(true);
+            }
+
+        });
+                
+        imagen.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                nombrePersonaje.setForeground(Color.red);
+                nombrePersonaje.setText("Rojo");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
+        
+        imagen2.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                nombrePersonaje.setForeground(Color.GREEN);
+                nombrePersonaje.setText("Verde");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
+        
+        imagen3.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                nombrePersonaje.setForeground(Color.GRAY);
+                nombrePersonaje.setText("Gris");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
+    
+    }
+    
+   
     
     /**
      * @param args the command line arguments
